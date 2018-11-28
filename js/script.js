deb54e57ce24a829e1b37239e8c2e0ef86823600
oscar ngolo <oscarngoloman@yahoo.com>
2018-11-28 07:44:47 +0300

    add js file in my web page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var add = function (number1, number2){
 return number1 + number2;
 };
-var number1 = parseInt(prompt("50"));
-var number2 = parseInt(prompt("15"));
+var number1 = parseInt(prompt("enter a number"));
+var number2 = parseInt(prompt("enter another number"));
 var result = add(number1, number2)
 alert(result);
